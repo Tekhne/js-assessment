342f66ca6f2f7022c984413e8aaf8a7414195ae2
Peter Havens <peter.havens@gmail.com>
2016-06-30 14:26:34 -0600

    Implement count().

--- app/count.js
+++ app/count.js
@@ -2,6 +2,13 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    let current = start;
 
+    let intervalID = setInterval(() => {
+      if (current > end) { clearInterval(intervalID) }
+      console.log(current++);
+    }, 100);
+
+    return { cancel: () => { clearInterval(intervalID) } };
   }
 };
