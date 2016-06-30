0359d6f950187df14858a413a4e666f0136628da
Peter Havens <peter.havens@gmail.com>
2016-06-30 13:09:11 -0600

    Implement async().

--- app/async.js
+++ app/async.js
@@ -2,7 +2,9 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    let deferred = $.Deferred();
+    setTimeout(() => { deferred.resolve(value) }, 1);
+    return deferred.promise();
   },
 
   manipulateRemoteData: function(url) {
