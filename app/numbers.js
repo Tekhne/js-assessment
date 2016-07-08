44ce741655d5e87891764c1a9e312e6f1007230f
Peter Havens <peter.havens@gmail.com>
2016-07-08 11:40:29 -0600

    Implement multiply().

--- app/numbers.js
+++ app/numbers.js
@@ -20,6 +20,24 @@ exports.numbersAnswers = {
   },
 
   multiply: function(a, b) {
+    function correctionFactor(number) {
+      if (number >= 1) { return 1; }
+      let exponent = (Math.floor(Math.log(number) / Math.log(10))) * -1;
+      return Math.pow(10, exponent);
+    }
 
+    let cf;
+    let cfa;
+    let cfb;
+
+    if ((a < 1) || (b < 1)) {
+      let cfa = correctionFactor(a);
+      let cfb = correctionFactor(b);
+      let cf = (cfa > cfb) ? cfa : cfb;
+
+      return ((a * cf) * (b * cf)) / Math.pow(cf, 2);
+    } else {
+      return a * b;
+    }
   }
 };
