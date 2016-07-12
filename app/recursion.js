aea048b11d2c25ec4dbf51c345f2cc2643070004
Peter Havens <peter.havens@gmail.com>
2016-07-11 19:31:15 -0600

    Implement listFiles().

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    let allFiles = [];
 
+    function recurse(_data) {
+      for (let i = 0; i < _data.files.length; i++) {
+        if (typeof _data.files[i] === 'string') {
+          allFiles.push(_data.files[i]);
+        } else {
+          recurse(_data.files[i]);
+        }
+      }
+    }
+
+    recurse(data);
+    return allFiles;
   },
 
   permute: function(arr) {
