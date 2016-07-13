040ee6e37b5e00ff2205f4437fa7546c2fd82489
Peter Havens <peter.havens@gmail.com>
2016-07-13 16:05:39 -0600

    Add ability to handle a specified dirName.

--- app/recursion.js
+++ app/recursion.js
@@ -4,17 +4,34 @@ exports.recursionAnswers = {
   listFiles: function(data, dirName) {
     let allFiles = [];
 
-    function recurse(_data) {
+    function record(_data) {
       for (let i = 0; i < _data.files.length; i++) {
         if (typeof _data.files[i] === 'string') {
           allFiles.push(_data.files[i]);
         } else {
-          recurse(_data.files[i]);
+          record(_data.files[i]);
         }
       }
     }
 
-    recurse(data);
+    function skip(_data) {
+      for (let i = 0; i < _data.files.length; i++) {
+        if (typeof _data.files[i] === 'string') {
+          continue;
+        } else if (_data.files[i].dir === dirName) {
+          record(_data.files[i]);
+        } else {
+          skip(_data.files[i]);
+        }
+      }
+    }
+
+    if (!dirName || (data.dir === dirName)) {
+      record(data);
+    } else {
+      skip(data);
+    }
+
     return allFiles;
   },
 
