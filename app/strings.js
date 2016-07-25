43e74d759ce2d09d34f698ea0373415506551661
Peter Havens <peter.havens@gmail.com>
2016-07-25 15:34:22 -0600

    Implement wordWrap().

--- app/strings.js
+++ app/strings.js
@@ -33,7 +33,39 @@ exports.stringsAnswers = {
   },
 
   wordWrap: function(str, cols) {
+    let characters = str.split('');
+    let line = 1;
+    let queue = [];
+    let result = [];
 
+    for (let i = 0; i < characters.length; i++) {
+      if (/\s/.test(characters[i])) {
+        if (line > cols) {
+          queue = queue.concat('\n');
+          result = Array.prototype.concat.apply(result, queue);
+          queue = [];
+          line = 1;
+        } else {
+          queue = queue.concat(characters[i]);
+          line++;
+        }
+      } else {
+        if (line > cols) {
+          for (let j = (queue.length - 1); j >= 0; j--) {
+            if (/\s/.test(queue[j])) {
+              queue[j] = '\n';
+              break;
+            }
+          }
+        }
+
+        queue = queue.concat(characters[i]);
+        line++;
+      }
+    }
+
+    result = Array.prototype.concat.apply(result, queue);
+    return result.join('');
   },
 
   reverseString: function(str) {
