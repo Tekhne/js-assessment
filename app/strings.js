7b24e52c2f588f57dcbcc0aa2b992cace974bf19
Peter Havens <peter.havens@gmail.com>
2016-07-25 15:33:42 -0600

    Implement reduceString().

--- app/strings.js
+++ app/strings.js
@@ -2,7 +2,34 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
+    let characters = str.split('');
+    let count = 0;
+    let result = [];
 
+    if (amount < 1) {
+      return '';
+    }
+
+    if (characters.length < 2) {
+      return str;
+    }
+
+    for (let i = 0; i < characters.length; i++) {
+      if (i === 0) {
+        result = result.concat(characters[i]);
+        count++;
+      } else if (characters[i - 1] === characters[i]) {
+        if (count < amount) {
+          result = result.concat(characters[i]);
+          count++;
+        }
+      } else {
+        result = result.concat(characters[i]);
+        count = 1;
+      }
+    }
+
+    return result.join('');
   },
 
   wordWrap: function(str, cols) {
